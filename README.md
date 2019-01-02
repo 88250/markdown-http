aed3d66306053fac9ad07c75158e4f1072210bdb
Van <lly219@gmail.com>
2019-01-02 12:46:14 +0800

    :tada:

--- README.md
+++ README.md
@@ -10,5 +10,5 @@
 
 1. 安装 Node.js
 2. 安装 marked：`npm install`
-3. `node http.js` 启动 marked（你可能需要 [nohup](https://hacpai.com/man?cmd=nohup)）。Marked 需要监听 `8250` 端口，所以启动前请确认该端口可用
+3. 启动 marked：`npm run start`。Marked 需要监听 `8250` 端口，所以启动前请确认该端口可用。你可能需要 [nohup](https://hacpai.com/man?cmd=nohup)
 4. 重启 Sym/Solo/Pipe 后就会使用 Marked 作为 markdown 渲染引擎了
---
+++ index.js
@@ -0,0 +1,84 @@
+/*
+ * Symphony - A modern community (forum/BBS/SNS/blog) platform written in Java.
+ * Copyright (C) 2012-2018, b3log.org & hacpai.com
+ *
+ * 本文件属于 Sym 商业版的一部分，请仔细阅读项目根文件夹的 LICENSE 并严格遵守相关约定
+ */
+
+/**
+ * @fileoverview marked HTTP server.
+ *
+ * @author <a href="http://vanessa.b3log.org">Liyuan Li</a>
+ * @author <a href="http://88250.b3log.org">Liang Ding</a>
+ * @version 1.2.0.1, Aug 20, 2018
+ * @since 1.7.0
+ */
+
+const http = require('http');
+const marked = require('marked');
+const renderer = new marked.Renderer();
+const PORT = 8250;
+
+renderer.listitem = function (text) {
+  if (text.indexOf('<input') > -1) {
+    return `<li class="content-reset__task">${text}</li>`;
+  }
+
+  return `<li>${text}</li>`;
+};
+
+marked.setOptions({
+  renderer: renderer,
+  gfm: true,
+  tables: true,
+  breaks: true,
+  smartLists: true,
+  highlight: function(code) {
+    return require('highlight.js').highlightAuto(code).value;
+  }
+});
+
+process.on('uncaughtException', function (err) {
+  console.log(err);
+});
+
+process.on('exit', function () {
+  console.log("exit");
+});
+
+process.on('SIGTERM', function () {
+  console.log("on signal [SIGTERM]");
+  process.exit(0);
+});
+
+process.on('SIGINT', function () {
+  console.log("on signal [SIGINT]");
+  process.exit(0);
+});
+
+process.on('SIGUSR1', function () {
+  console.log("on signal [SIGUSR1]");
+  process.exit(0);
+});
+
+process.on('SIGUSR2', function () {
+  console.log("on signal [SIGUSR2]");
+  process.exit(0);
+});
+
+const server = http.createServer(function (request, response) {
+  let mdContent = '';
+
+  request.on('data', function (data) {
+    mdContent += data;
+  });
+
+  request.on('end', function () {
+    response.write(marked(mdContent));
+
+    response.end();
+  });
+});
+
+server.listen(PORT);
+console.log("Marked engine is running at port: " + PORT);
